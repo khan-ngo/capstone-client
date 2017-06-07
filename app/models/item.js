3a785c1f6dd1b5b50ee27a32fa48524294e8a307
kqngo <khan.ngo@gmail.com>
2017-06-07 18:34:49 -0400

    Add contact form for user to initiate chat with posted item
    For those interested in a particular post, this form will initiate the contact.

--- app/models/item.js
+++ app/models/item.js
@@ -4,7 +4,6 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   location: DS.attr('string'),
   body: DS.attr('string'),
-  address: DS.attr('string'),
   category: DS.belongsTo('category'),
   user: DS.belongsTo('user'),
   editable: DS.attr('boolean')
