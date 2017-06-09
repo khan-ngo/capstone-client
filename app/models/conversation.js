b4bcfa74dc56e08c33731dc8bddd60c52c822bef
kqngo <khan.ngo@gmail.com>
2017-06-09 01:15:45 -0400

    Correct typo in conversation.js

--- app/models/conversation.js
+++ app/models/conversation.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  owner: DS.attr('number'),
-  respondent: DS.attr('number'),
+  user1: DS.attr('number'),
+  user2: DS.attr('number'),
   messages: DS.hasMany('message')
 });
